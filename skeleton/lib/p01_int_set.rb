bc4d5564510cef2f7e065921fc88184e92cb89e0
Truong <trnguyen34@gmail.com>
2022-07-11 15:51:27 -0700

    finished with p01 & p02

--- skeleton/lib/p01_int_set.rb
+++ skeleton/lib/p01_int_set.rb
@@ -1,44 +1,65 @@
-class MaxIntSet
+class MaxIntSet 
+  attr_reader :max
+  attr_accessor :store
   def initialize(max)
+    @max = max
+    @store = Array.new(@max)
   end
 
   def insert(num)
+    is_valid?(num)
+    @store[num] = true
   end
 
   def remove(num)
+    raise "Out of bounds" if num < 0 || num > max
+    @store[num] = false
   end
 
   def include?(num)
+    @store[num]
   end
 
   private
 
   def is_valid?(num)
+    raise "Out of bounds" if num < 0 || num > max
   end
 
   def validate!(num)
+
   end
 end
 
 
 class IntSet
+  attr_reader :num_buckets
+  attr_accessor :store
   def initialize(num_buckets = 20)
+    @num_buckets = num_buckets
     @store = Array.new(num_buckets) { Array.new }
   end
 
   def insert(num)
+    key = num % num_buckets
+    @store[key] << num
   end
 
   def remove(num)
+    key = num % num_buckets
+    @store[key].delete(num)
   end
 
   def include?(num)
+    key = num % num_buckets
+    @store[key].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    key = num % num_buckets
   end
 
   def num_buckets
@@ -48,19 +69,40 @@ end
 
 class ResizingIntSet
   attr_reader :count
+  attr_accessor :store, :num_buckets
 
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
     @count = 0
+    @num_buckets = num_buckets
   end
 
   def insert(num)
+    if @count == @num_buckets
+      self.resize!
+    end
+
+    key = num % num_buckets
+    if self.include?(num) == false
+      @store[key] << num
+      @count += 1
+    end
   end
 
   def remove(num)
+    return @count if !self.include?(num)
+    key = num % num_buckets
+    @store[key].delete(num)
+    @count -= 1
   end
 
   def include?(num)
+    key = num % @num_buckets
+    @store[key].each do |ele|
+      return true if ele == num
+    end
+
+    false
   end
 
   private
@@ -74,5 +116,15 @@ class ResizingIntSet
   end
 
   def resize!
+    new_store = Array.new(@num_buckets*2) {Array.new}
+    @store.each do |subarray|
+     subarray.each do |ele|
+        key = ele % (@num_buckets*2)
+        new_store[key] << ele
+      end
+    end
+
+    @num_buckets *= 2
+    @store = new_store
   end
 end
--- skeleton/lib/p02_hashing.rb
+++ skeleton/lib/p02_hashing.rb
@@ -1,14 +1,29 @@
 class Integer
   # Integer#hash already implemented for you
+
 end
 
 class Array
   def hash
+    sum = 0
+    self.each_with_index do |ele, i|
+      el = ele + (i + 2)
+      sum += el.hash
+    end
+
+    sum
   end
 end
 
 class String
   def hash
+    sum = 0
+    self.each_char.with_index do |char, i|
+      el = char.ord + (i + 2)
+      sum += el.hash
+    end
+
+    sum
   end
 end
 
@@ -16,6 +31,11 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    sum = 0
+    self.each do |key, value|
+      sum += key.hash + value.hash
+    end
+
+    sum
   end
 end
